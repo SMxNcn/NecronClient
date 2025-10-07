c8810eb6dd7f8bbe5316a512c251433346310c29
Necron <1254256257@qq.com>
2025-10-07 23:53:35 +0800

    简化FakeWipe的聊天栏消息

--- src/main/java/cn/boop/necron/module/impl/FakeWipe.java
+++ src/main/java/cn/boop/necron/module/impl/FakeWipe.java
@@ -4,15 +4,12 @@ import cc.polyfrost.oneconfig.utils.hypixel.HypixelUtils;
 import cn.boop.necron.Necron;
 import cn.boop.necron.utils.LocationUtils;
 import net.minecraft.client.gui.GuiScreenBook;
-import net.minecraft.event.ClickEvent;
-import net.minecraft.event.HoverEvent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.ChatStyle;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -92,13 +89,7 @@ public class FakeWipe {
             }
 
             Necron.mc.displayGuiScreen(new GuiScreenBook(Necron.mc.thePlayer, book, false));
-
-            Necron.mc.thePlayer.addChatMessage(new ChatComponentText("§eYour SkyBlock Profile §bNecron §c§lhas been wiped §r§eas co-op member was determined to be boosting or cheating.\n§eIf you believe this to be in error, you can contact our support team:"));
-            Necron.mc.thePlayer.addChatMessage(new ChatComponentText("§b§nsupport.hypixel.net")
-                    .setChatStyle(new ChatStyle()
-                            .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText("§eClick to open!")))
-                            .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://support.hypixel.net"))
-                            .setUnderlined(true)));
+            Necron.mc.thePlayer.addChatMessage(new ChatComponentText("§eYour SkyBlock Profile §bNecron §c§lhas been wiped §r§eas co-op member was determined to be boosting or cheating.\n§eIf you believe this to be in error, you can contact our support team: §b§nsupport.hypixel.net"));
         });
     }
 }
