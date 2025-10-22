d9f5fb5867afc8020fa6baace83e7ae1108cfe4b
Necron <1254256257@qq.com>
2025-10-22 21:51:09 +0800

    添加DungeonESP和M7 P5路径点

--- src/main/java/cn/boop/necron/config/impl/DungeonOptionsImpl.java
+++ src/main/java/cn/boop/necron/config/impl/DungeonOptionsImpl.java
@@ -1,7 +1,9 @@
 package cn.boop.necron.config.impl;
 
+import cc.polyfrost.oneconfig.config.annotations.Color;
 import cc.polyfrost.oneconfig.config.annotations.Number;
 import cc.polyfrost.oneconfig.config.annotations.Switch;
+import cc.polyfrost.oneconfig.config.core.OneColor;
 import cn.boop.necron.config.ModConfig;
 
 public class DungeonOptionsImpl extends ModConfig {
@@ -27,4 +29,17 @@ public class DungeonOptionsImpl extends ModConfig {
     public static boolean hasDaemon = false;
     @Number(name = "Daemon shard level", min = 1, max = 10, subcategory = "RNG Meter")
     public static int daemonLevel = 1;
+
+    @Switch(name = "P5 Waypoints", subcategory = "Dungeon")
+    public static boolean m7Waypoints = false;
+    @Switch(name = "Only P5", subcategory = "Dungeon")
+    public static boolean onlyP5 = false;
+    @Switch(name = "Bat ESP", subcategory = "Dungeon")
+    public static boolean batESP = false;
+    @Switch(name = "Wither ESP", subcategory = "Dungeon")
+    public static boolean witherESP = false;
+    @Color(name = "ESP Color", description = "Color of dungeon ESP", subcategory = "Dungeon")
+    public static OneColor espColor = new OneColor(255, 182, 43);
+    @Switch(name = "Smooth ESP", description = "May display on wrong location", subcategory = "Dungeon")
+    public static boolean smoothESP = false;
 }
