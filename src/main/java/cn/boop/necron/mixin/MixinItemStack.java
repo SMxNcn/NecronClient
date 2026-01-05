e28785915e4a4556f1547af6ef1ba9ecb8d1a22a
Necron <1254256257@qq.com>
2026-01-05 19:59:02 +0800

    移除了多余的非颜色信息

--- src/main/java/cn/boop/necron/mixin/MixinItemStack.java
+++ src/main/java/cn/boop/necron/mixin/MixinItemStack.java
@@ -4,26 +4,42 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StatCollector;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 @Mixin(ItemStack.class)
 public class MixinItemStack {
-    @ModifyVariable(method = "getTooltip", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NBTTagCompound;getCompoundTag(Ljava/lang/String;)Lnet/minecraft/nbt/NBTTagCompound;", ordinal = 0), ordinal = 0, argsOnly = true)
-    private boolean forceShowColor(boolean advanced, EntityPlayer playerIn, boolean originalAdvanced) {
-        ItemStack stack = (ItemStack) (Object) this;
+    @Inject(method = "getTooltip", at = @At("RETURN"))
+    private void replaceDyedWithColor(EntityPlayer playerIn, boolean advanced, CallbackInfoReturnable<List<String>> cir) {
+        ItemStack self = (ItemStack) (Object) this;
 
-        if (stack.getItem() instanceof ItemArmor) {
-            ItemArmor armor = (ItemArmor) stack.getItem();
-            if (armor.getArmorMaterial() == ItemArmor.ArmorMaterial.LEATHER) {
-                NBTTagCompound nbt = stack.getTagCompound();
-                if (nbt != null && nbt.hasKey("display", 10) && nbt.getCompoundTag("display").hasKey("color", 3)) {
-                    return true;
-                }
+        if (!(self.getItem() instanceof ItemArmor)) return;
+        ItemArmor armor = (ItemArmor) self.getItem();
+        if (armor.getArmorMaterial() != ItemArmor.ArmorMaterial.LEATHER) return;
+
+        NBTTagCompound nbt = self.getTagCompound();
+        if (nbt == null || !nbt.hasKey("display", 10)) return;
+
+        NBTTagCompound display = nbt.getCompoundTag("display");
+        if (!display.hasKey("color", 3)) return;
+
+        int color = display.getInteger("color");
+        String colorLine = "Color: #" + String.format("%06X", color & 0xFFFFFF);
+
+        List<String> tooltip = cir.getReturnValue();
+        String dyedText = EnumChatFormatting.ITALIC + StatCollector.translateToLocal("item.dyed");
+
+        for (int i = 0; i < tooltip.size(); i++) {
+            if (tooltip.get(i).equals(dyedText)) {
+                tooltip.set(i, colorLine);
+                break;
             }
         }
-
-        return advanced;
     }
 }
